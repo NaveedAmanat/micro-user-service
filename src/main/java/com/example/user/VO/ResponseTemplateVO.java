8211d5d5a7b739dc0d2a02feb0001daa322b3326
NaveedAmanat <44110424+NaveedAmanat@users.noreply.github.com>
2022-12-03 17:16:00 +0500

    Update ResponseTemplateVO.java

--- src/main/java/com/example/user/VO/ResponseTemplateVO.java
+++ src/main/java/com/example/user/VO/ResponseTemplateVO.java
@@ -7,8 +7,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+// @AllArgsConstructor
+// @NoArgsConstructor
 public class ResponseTemplateVO {
 
     private User user;
